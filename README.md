68384f5374514f0ea611fb33308a525aed3690bf
Mehul Agarwal <ipofmehul@gmail.com>
2023-03-18 02:24:46 -0400

    convert to pytube

--- README.md
+++ README.md
@@ -7,10 +7,23 @@ Table of Contents
 
 # Installation Instructions
 
-1. Install required packages with pip
+1. Python version needs to be below 3.11
+
+2. Install required packages with pip
 
 `pip install -r requirements.txt`
 
 2. Run a program with Python3
 
 `python3 transcriber.py -u, --url <URL>`
+
+3. pytube requires pip installation 
+
+4. ffmpeg needs to be pip installed if not already present
+
+5. in "LOCAL_PATH_TO/pytube/cipher.py",
+Change this line:
+411:     transform_plan_raw = find_object_from_startpoint(raw_code, match.span()[1] - 1)
+to this:
+152: transform_plan_raw = js
+
--- transcriber.py
+++ transcriber.py
@@ -8,13 +8,18 @@ import re
 import sys
 import torch
 import whisper
+import os
+from pathlib import Path
 
 from googletrans import Translator
-import youtube_dl
-
+# import youtube_dl
+from pytube import YouTube
 
 AUDIOFILE = "audio.mp3"  # Save audio file as audio.mp3
-
+outdir = ""  # Save audio file to current working directory
+lyrics = []
+no_lyrics = []
+total_clip_length = 0.0
 
 def match_pattern(pattern, arg):
     """Convert it to normal video URL if YouTube shorts URL is given."""
@@ -40,15 +45,28 @@ def get_audio():
     for opt, arg in opts:
         if opt in ['-u', '--url']:
             url = match_pattern("shorts/", arg)
-    video_info = youtube_dl.YoutubeDL().extract_info(url=url, download=False)
-    options = {
-        'format': 'bestaudio/best',
-        'keepvideo': False,
-        'outtmpl': AUDIOFILE,
-    }
-    with youtube_dl.YoutubeDL(options) as ydl:
-        ydl.download([video_info['webpage_url']])
 
+    yt = YouTube(url)
+    ##@ Extract audio with 160kbps quality from video
+    video = yt.streams.filter(abr='160kbps').last()
+
+    ##@ Downloadthe file
+    out_file = video.download(output_path=outdir)
+    base, ext = os.path.splitext(out_file)
+    # new_file = Path(f'{base}.mp3')
+    new_file = Path('audio.mp3')
+    os.rename(out_file, new_file)
+    ##@ Check success of download
+    if new_file.exists():
+        print(f'{yt.title} has been successfully downloaded.')
+    else:
+        print(f'ERROR: {yt.title}could not be downloaded!')
+    
+    global total_clip_length
+    total_clip_length = float(yt.length)
+    print("Total clip length in seconds: " + str(total_clip_length))
+    print("Total clip length: " + str(int(total_clip_length // 60)) + ":" + str(int(total_clip_length % 60)))
+    AUDIOFILE = new_file
 
 def banner(text):
     """Display a message when the script is working in the background"""
@@ -69,8 +87,75 @@ def get_result():
     model_name = input("Select speech recognition model name (tiny, base, small, medium, large): ")
     banner("Transcribing text")
     model = whisper.load_model(model_name, device=check_device())
-    result = model.transcribe(AUDIOFILE)
-    format_result('transcription.txt', result["text"])
+    sys.stdout = open("transcription.txt", "w")
+    print("Clip length: " + str(total_clip_length))
+    result = model.transcribe(AUDIOFILE, verbose=True)
+
+    # result = model.transcribe(AUDIOFILE)
+    
+    # format_result('transcription.txt', result["text"])
+
+def timestamp_to_seconds(time_str):
+    """Convert timestamp to seconds."""
+    timestamp = time_str.split(':')
+    time = float(timestamp[0]) * 60 + float(timestamp[1])
+    return time
+
+def create_lyric_timestamps():
+    # open and read transcription.txt line by line
+    # get global total clip length
+    global total_clip_length
+    # print("Total clip length in seconds: " + str(total_clip_length))
+    sys.stdout = sys.__stdout__
+    print("Creating lyric timestamps")
+    lyric_file = open('transcription.txt', 'r')
+    lines = lyric_file.readlines()
+    lyric_file.close()
+
+    # create a list of lyrics with timestamps
+    for i in range(len(lines)):
+        if(i == 0):
+            total_clip_length = float(lines[i].split('Clip length: ')[1])
+            continue
+        if (i == 1 or i == 2):
+            continue
+        if(lines[i] == '\n' or lines[i] == ' ' or lines[i] == '[]'):
+            continue
+        else:
+            # get timestamp 1 and get timestamp 2 from input like "[00:00.000 --> 00:29.920] I'm in my room, it's a typical Tuesday night, I'm listening to the kind of music she doesn't"
+            # first split on [] and then split on -->
+            timestamps = lines[i].split('[')[1].split(']')[0]
+            # remove spaces from timestamp
+            timestamp1 = timestamps.split('-->')[0].replace(" ", "")
+            # convert timestamp to seconds
+            t1 = timestamp_to_seconds(timestamp1)
+
+            timestamp2 = timestamps.split('-->')[1].replace(" ", "")
+            # convert timestamp to seconds
+            t2 = timestamp_to_seconds(timestamp2)
+
+            # remove space from beginning and end of sentence
+            sentence = lines[i].split(']')[1].strip()
+            lyrics.append([t1, t2, sentence])
+    sys.stdout = sys.__stdout__
+
+    # Calculate empty space between timestamps
+    last_end_time = 0
+    for i in range(len(lyrics)):
+        t1 = lyrics[i][0]
+        t2 = lyrics[i][1]
+        if (t1 > last_end_time):
+            no_lyrics.append([last_end_time, t1])
+        last_end_time = t2
+        if (i == (len(lyrics) - 1)):
+            # print("I'm here 1")
+            if (t2 < total_clip_length):
+                # print("I'm here 2")
+                no_lyrics.append([t2, total_clip_length])
+    print("Lyrics: ")
+    print(lyrics)
+    print("Parts of song with no lyrics: ")
+    print(no_lyrics)
 
 
 def format_result(file_name, text):
@@ -103,6 +188,7 @@ def main():
     """Main function."""
     get_audio()  # Download an mp3 audio file to transcribe to text
     get_result()  # Get audio transcription and translation if needed
+    create_lyric_timestamps()
 
 
 if __name__ == "__main__":
